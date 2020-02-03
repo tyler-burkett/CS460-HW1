d158dd800e4c7b7e8753c1cfb33438ff4e1ccc8c
Tyler Burkett <36940296+tyler-burkett@users.noreply.github.com>
2020-02-02 22:14:49 -0500

    Implemented build_tree method

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,8 @@
-import numpy as np
+import pandas as pd
 from anytree import AnyNode
+from info_theory import info_gain
+from pandas_util import values_of, subset_by_value
+
 
 class DecisionTree:
     """Decsion Tree Using ID3 Algorithm"""
@@ -8,7 +11,7 @@ class DecisionTree:
         pass
 
     def fit(self, training_data, limit=None):
-        self.root = build_tree(training_data, limit)
+        self.root = self.build_tree(training_data, limit)
 
     def predict(self, examples):
         pass
@@ -17,7 +20,7 @@ class DecisionTree:
         """Recursive function to build decision tree
 
         Parameters:
-        training_data - Pandas Dataframe; last column is taken as the class class_labels
+        training_data - Pandas DataFrame; last column is taken as the class labels
 
         Keyword Args:
         limit - Max depth limit of tree process; None by default
@@ -25,13 +28,49 @@ class DecisionTree:
         node = AnyNode()
 
         # Data is pure, create leaf of tree with class label
-        if len(set(training_data.iloc[:,-1])) == 1:
-            node.label = training_data.iloc[1,-1]
+        if len(set(training_data.iloc[:, -1])) == 1:
+            node.label = training_data.iloc[1, -1]
             return node
 
-        # No more attributes to split on; use most common label as class label
+        # No more features to split on; use most common label as class label
         if len(training_data.columns == 1):
-            node.label = max(set(training_data.iloc[:,-1]), key = list(training_data.iloc[:,-1]).count)
+            node.label = max(
+                set(training_data.iloc[:, -1]),
+                key=list(training_data.iloc[:, -1]).count)
             return node
 
-        # Begin splitting on attribute
+        # Default; begin tree splitting
+        # Determine feature that gives best information gain
+        split_feature = max(training_data.columns[0:-1],
+                            default=training_data.columns[0],
+                            key=lambda x: info_gain(training_data, x))
+        node.attribute = split_feature
+
+        # Determine possible values for splitting feature and
+        # create leaves/subtrees
+        values = values_of(training_data, split_feature)
+        node.map = dict()
+        for value in values:
+            # Create subset with feature removed
+            training_data_v = subset_by_value(training_data, split_feature, value)
+            training_data_v.drop(split_feature, axis=1)
+
+            # Subset data based on value
+            if training_data_v.empty or limit < 1 or limit is None:
+                # subset is empty; create child leaf with label of the
+                # most common class label
+                child = AnyNode()
+                child.label = max(
+                    set(training_data.iloc[:, -1]),
+                    key=list(training_data.iloc[:, -1]).count)
+            else:
+                # subset is not empty; create child subtree recursively
+                new_limit = limit - 1
+                child = self.build_tree(training_data_v, new_limit)
+
+            # Add new node as child of the current node and
+            # map value to this child
+            node.children = list(node.children) + [child]
+            node.map[value] = child
+
+        return node
